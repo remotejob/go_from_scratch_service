9ea8d1b78bdc6a17fb1c2ca57b8e853a921b8edb
remotejob <support@mazurov.eu>
2021-01-10 22:37:26 +0200

    dev

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/remotejob/go_from_scratch_service
+
+go 1.14
---
+++ useful.bash
@@ -0,0 +1,15 @@
+docker build -t mygolangapp:using-alpine .
+
+docker images "mygolangapp:*"
+
+
+docker run mygolangapp:using-alpine
+
+
+docker build -t mygolangapp:using-multistage -f Dockerfile.multi .
+
+git remote set-url origin https://remotejob:<password>@github.com/remotejob/go_from_scratch_service
+
+
+
+
